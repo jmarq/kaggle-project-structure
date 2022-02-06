5599fb3f47119392bbe332eec6637ababa7e3a07
Jimbo <marquardtjames@gmail.com>
2022-02-06 13:43:37 -0600

    import traning file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from titanic.models import model1
-
-model1.run_model()
+import os
+training_filename = "train.csv"
+results_filename = "results.csv"
+# filename=os.path.abspath(filename)
+results = model1.run_model(training_filename, results_filename)
--- titanic/models/model1.py
+++ titanic/models/model1.py
@@ -2,9 +2,12 @@ from re import T
 from titanic.features.transform import transform_data
 
 
-def run_model():
-    data = [1,2,3]
+def run_model(training_filename, results_filename):
+    data = open(training_filename,'r').read()
     data = transform_data(data)
     print("running model")
     print(data)
+    outfile = open(results_filename, 'w')
+    outfile.write(data)
+    outfile.close()
     return data
