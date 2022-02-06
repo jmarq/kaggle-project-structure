18bdcfe5b9bf1cb89e1fe89c722d8f0b981aa9be
Jimbo <marquardtjames@gmail.com>
2022-02-06 17:33:58 -0600

    comments. added jupyter notebook

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ training_filename = "train.csv"
 results_filename = "results.csv"
 # filename=os.path.abspath(filename)
 results = model1.run_model(training_filename, results_filename)
+
+# maybe something like this?
+# myModel = model1.MyModel()
+# myModel.train(training_filename)
+# myModel.classify(unlabeled_filename)
+
+# though at what point are you just re-hashing functionlity provided by these model libraries?
+# what is the benefit of your own class?
+#  repeatable functionality common to the kaggle challenge
+#  class can hold a stateful member like onehotencoder or standardscaler whose info needs to persist on subsequent calls?
+#    would this maybe be better for a "transformer" class rather than a model class?
--- titanic/models/model1.py
+++ titanic/models/model1.py
@@ -2,6 +2,11 @@ from re import T
 from titanic.features.transform import transform_data
 
 
+# maybe a class here?
+# class MyModel(Model):
+#   def train(self, data)
+#   def classify(self, data)
+
 def run_model(training_filename, results_filename):
     data = open(training_filename,'r').read()
     data = transform_data(data)
